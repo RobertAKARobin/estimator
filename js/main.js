d9f8b7e3736db445500ebf615bc84db3e2e84315
Robin Thomas <>
2017-07-13 10:34:32 -0500

    Boilerplate code for classes

--- js/main.js
+++ js/main.js
@@ -1,15 +1,50 @@
 'use strict';
 
-var Table = (function(){
+var Project = (function(){
 
-	return {
-		view: function(){
-			return 'Component'
+	// Everything is private, unless attached to $Class or $Instance
+	var $Class = {};
+	var $Instance = {};
+	var $Instance_Constructor;
+	var $DOM_Constructor;
+
+	$Class.all = [];
+	// Class methods
+	$Class.new = function(){
+		var project = Object.create($Instance);
+		$Instance_Constructor.apply(project, arguments);
+		$DOM_Constructor.apply(project);
+		$Class.all.push(project);
+		return project;
+	}
+
+	$Instance_Constructor = function(title){
+		var project = this;
+		project.title = title;
+	}
+	// Instance methods
+	$Instance.mountTo = function(element){
+		var project = this;
+		return m.mount(element, project.DOM);
+	}
+
+	// Creates a unique Mithril component for each instance
+	$DOM_Constructor = function(){
+		var project = this;
+		project.DOM = {
+			view: function(){
+				return [
+					m('h1', project.title)
+				]
+			}
 		}
 	}
+
+	return $Class;
 
 })();
 
 document.addEventListener('DOMContentLoaded', function(){
-	m.mount(document.getElementById('project'), Table);
+	var project = Project.new('My sweet project');
+	project.mountTo(document.getElementById('project'));
 });
